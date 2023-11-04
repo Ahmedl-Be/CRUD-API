e77f95253587447bc96669afbb93c96050e841fa
Ahmed <ahmedabdallabeltagy@gmail.com>
2023-11-04 06:47:49 +0200

    user login & register

--- routes/courses.route.js
+++ routes/courses.route.js
@@ -1,15 +1,27 @@
 const express = require("express")
 const controllers = require("../controllers/courses.controllers")
 const {vaildationSchema} = require("../middlewares/vaildationSchema")
+const verifyToken= require("../middlewares/verifyToken")
+const userRoles = require("../utils/userRoles")
+const allowedTo = require("../middlewares/allowedTo")
 const router = express.Router()
 
 router.route("/")
             .get(controllers.getAllCourses)
-            .post(vaildationSchema(),controllers.createNewCourse)
+            .post(
+                verifyToken,
+                allowedTo(userRoles.MANGER),
+                vaildationSchema(),
+                controllers.createNewCourse
+            )
 
 router.route('/:courseId')
             .get(controllers.getSingleCourse)
             .patch(controllers.updateCourse)
-            .delete(controllers.deleteCourse)
+            .delete(
+                verifyToken,
+                allowedTo(userRoles.ADMIN,userRoles.MANGER),
+                controllers.deleteCourse
+            )
 
 module.exports = router
